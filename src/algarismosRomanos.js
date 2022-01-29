c1ad9b50619e71af855607ec55f7ead1e6d3ef59
lucsimao <lucasimao64@gmail.com>
2022-01-29 00:39:55 -0300

    refactor: simplicar as condicionais

--- src/algarismosRomanos.js
+++ src/algarismosRomanos.js
@@ -1,11 +1,6 @@
 const converteParaAlgarismoRomano = (numero) => {
-  if (numero === 1) {
-    return 'I';
-  } else {
-    if (numero === 2) {
-      return 'II';
-    }
-  }
+  if (numero === 1) return 'I';
+  if (numero === 2) return 'II';
 };
 
 module.exports = { converteParaAlgarismoRomano };
