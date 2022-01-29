23da8262980423636ef353c6464c84cbdf7c2356
lucsimao <lucasimao64@gmail.com>
2022-01-29 00:41:35 -0300

    feat: garantir retornar III quando o valor recebido for 3

--- src/algarismosRomanos.js
+++ src/algarismosRomanos.js
@@ -1,6 +1,7 @@
 const converteParaAlgarismoRomano = (numero) => {
   if (numero === 1) return 'I';
   if (numero === 2) return 'II';
+  if (numero === 3) return 'III';
 };
 
 module.exports = { converteParaAlgarismoRomano };
