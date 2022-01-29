c4451adfc05b2be5a83d1dee7d8bdcca9b677a9b
lucsimao <lucasimao64@gmail.com>
2022-01-29 00:48:23 -0300

    feat: garantir retornar IV quando o valor recebido for 4

--- src/algarismosRomanos.js
+++ src/algarismosRomanos.js
@@ -1,4 +1,4 @@
-const algarismosRomanos = ['INVALIDO', 'I', 'II', 'III'];
+const algarismosRomanos = ['INVALIDO', 'I', 'II', 'III', 'IV'];
 
 const converteParaAlgarismoRomano = (numero) => {
   return algarismosRomanos[numero];
