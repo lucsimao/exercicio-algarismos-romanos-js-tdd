80012a1b4eb32dedfa38e5c734f779860d5d8335
lucsimao <lucasimao64@gmail.com>
2022-02-06 21:17:10 -0300

    refactor: extract processaAlgarismoAntesDoMaior

--- src/services/converteAlgarismo.ts
+++ src/services/converteAlgarismo.ts
@@ -1,4 +1,4 @@
-import { Collection } from './../interfaces/Collection';
+import { Collection } from '../interfaces/Collection';
 export const converteAlgarismo = (
   coleção: Collection,
   numero: number
@@ -13,24 +13,43 @@ export const converteAlgarismo = (
     if (numero === coleção[chave]) {
       return chave;
     }
-    if (numero > coleção[chave]) chavesAtuais[chave] = coleção[chave];
+    if (numero > coleção[chave]) {
+      chavesAtuais[chave] = coleção[chave];
+    }
     if (numero < coleção[chave]) break;
   }
 
   const algarismoRomano = chave || 'I';
 
   if (numero < coleção[chave]) {
-    const chavesAuxiliares = Object.keys(chavesAtuais);
-    for (let chaveAuxiliar of chavesAuxiliares) {
-      const diferença = coleção[chave] - numero;
-      if (diferença === chavesAtuais[chaveAuxiliar]) {
-        return converteAlgarismo(chavesAtuais, diferença) + algarismoRomano;
-      }
-    }
-    return converteAlgarismo(chavesAtuais, numero);
+    const algarismoAntesDoMaior = processaAlgarismoAntesDoMaior(
+      chavesAtuais,
+      coleção,
+      chave,
+      numero
+    );
+
+    return algarismoAntesDoMaior || converteAlgarismo(chavesAtuais, numero);
   }
   const valorDoAlgarismo = coleção[chave] || 1;
   const diferençaDoNumero = numero - valorDoAlgarismo;
 
   return algarismoRomano + converteAlgarismo(coleção, diferençaDoNumero);
+};
+
+const processaAlgarismoAntesDoMaior = (
+  chavesAtuais: Collection,
+  coleção: Collection,
+  chave: string,
+  numero: number
+) => {
+  const algarismoRomano = chave || 'I';
+  const chavesAuxiliares = Object.keys(chavesAtuais);
+  for (let chaveAuxiliar of chavesAuxiliares) {
+    const diferença = coleção[chave] - numero;
+    if (diferença === chavesAtuais[chaveAuxiliar]) {
+      return converteAlgarismo(chavesAtuais, diferença) + algarismoRomano;
+    }
+  }
+  return null;
 };
